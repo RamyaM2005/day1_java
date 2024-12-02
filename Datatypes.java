659128708d249527c5374f5371536d2874a449d3
RamyaM2005 <ramyareddym023@gmail.com>
2024-12-02 14:38:56 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,12 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        int a=2;
+        float f=1.4f;
+        double d=2.3;
+        char c='r';
+        System.out.println(a);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+    }
+}
